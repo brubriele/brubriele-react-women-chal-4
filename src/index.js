98eed48565893dd28f237a45a6e396e364ca890b
Bruna Gabriele Paula <bgabriel@everis.com>
2020-06-11 18:07:30 -0300

    Function getShoppingCart sem validação dos testes

--- src/index.js
+++ src/index.js
@@ -1,40 +1,76 @@
-const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
 const productsList = require('./data/products.json')
-let mockIds = [120, 230, 230, 230]
-let cart = []
+const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
+let promotionType = [];
+let finalPurchases = [];
+let regularPriceTotal = [];
 
-const filterId = (productsList) => {
-	let list = productsList.products
+const getShoppingCart = (productList, products) => {
+	const selectedItems = products.map(item => productsList.products.filter(product => product.id === item))
+	const cart = {}
 
-	for (let el of mockIds) {
-		let filtered = list.filter(product => product.id === el)
-		listItems(filtered)
-	}
+	displaysOrder(selectedItems, cart)
+	calculetedPurchase(productList, products, cart)
 }
 
-const listItems = (filtered) => {
-	const list = filtered.map(elem => (
-		{
-			name: elem.name,
-			category: elem.category
-		}
-	));
-
-	cart.push(list)
+const displaysOrder = (selectedItems, cart) => {
+	let objProducts = selectedItems.map(elem => ({
+		name: elem[0].name,
+		category: elem[0].category
+	}));
+	Object.assign(cart, {
+		products: objProducts
+	})
+	setPromotion(objProducts, cart)
 }
 
-const setPromotion = (cart) => {
-	let products = cart.reduce((acc, val) => acc.concat(val), []);
-	let categories = products.map(el => el.category);
-	let setPromotion = categories.filter(function (v, i) { return i == categories.lastIndexOf(v); }).length - 1
+const setPromotion = (objProducts, cart) => {
+	let categories = objProducts.map(el => el.category)
+	let setPromotion = categories.filter(function (v, i) {
+		return i == categories.lastIndexOf(v);
+	}).length - 1
 	let promotion = promotions[setPromotion]
 
-	console.log(promotions[setPromotion])
-	console.log(products, ',', promotions[setPromotion])
+	Object.assign(cart, {
+		promotion: promotion
+	})
+	promotionType.push(promotion)
 }
 
-filterId(productsList)
-setPromotion(cart)
+const calculetedPurchase = (productsList, products, cart) => {
+	for (let item of products) {
+		let orderedProducts = productsList.products.filter(product => product.id === item)
+		let hasDiscount = orderedProducts[0].promotions.filter(product => product.looks.includes(...promotionType))
+		let regularPrice = orderedProducts.map(price => price.regularPrice)
+		let finalPurchase = !hasDiscount.length ? regularPrice : hasDiscount.map(promotion => promotion.price)
 
+		finalPurchases.push(Number(finalPurchase));
+		regularPriceTotal.push(Number(regularPrice))
+	}
+
+	let totalPriceWithDiscount = finalPurchases.reduce(function (acc, current) {
+		return acc + current;
+	}, 0);
+	let discountAmount = regularPriceTotal.reduce((acc, curr) => acc + curr) - totalPriceWithDiscount
+
+	Object.assign(cart, {
+		totalPrice: totalPriceWithDiscount.toFixed(2),
+		discountValue: discountAmount.toFixed(2)
+	})
+
+	calculateDiscount(totalPriceWithDiscount, cart)
+}
+
+const calculateDiscount = (totalPriceWithDiscount, cart) => {
+	let regularPriceAmount = regularPriceTotal.reduce((acc, curr) => acc + curr)
+	let deferredAmount = ((regularPriceAmount - totalPriceWithDiscount) * 100 / regularPriceAmount).toFixed(2) + '%'
+
+	Object.assign(cart, {
+		discount: deferredAmount
+	})
+
+	console.log(cart)
+}
+
+getShoppingCart(productsList, [130, 140, 230, 260])
 
 // module.exports = { getShoppingCart };
